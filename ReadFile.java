dcb0f0dcf29360c8c5882687a3a6c86413ae0975
Alex Yang <a.yang2@share.epsb.ca>
2018-04-21 16:48:52 +0000

    Added file reader file

---
+++ ReadFile.java
@@ -0,0 +1,21 @@
+package textfiles;
+
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+public class ReadFile
+{
+	private String path;
+
+	public ReadFile (String file_path)
+	{
+		path = file_path;
+	}
+
+	public String[] OpenFile() throws IOException {
+
+	}
+
+
+}
