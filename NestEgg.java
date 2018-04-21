005b9a942a929be0bfc3adc3398a4948bc9e165a
Alex Yang <a.yang2@share.epsb.ca>
2018-04-21 15:26:49 +0000

    Added switch statement for args

--- NestEgg.java
+++ NestEgg.java
@@ -9,7 +9,18 @@ public class NestEgg
 		//VARIABLES : salary, save, growthRate, years, infla
 		for(int i = 0; i < args.length; i++)
 		{
-		
+		switch (i) {
+			case 0: double salary = args[i] //amount user makes yearly
+				break;
+			case 1: double save = args[i] //how much user will put into account each year
+				break;
+			case 2: double growthRate = args[i] //percentage that account will increase by yearly
+				break;
+			case 3: int years = args[i] //amount of years program runs for
+				break;
+			case 4: double infla = args[i] //inflation rate
+			
+			}
 		}
 		
 	
@@ -19,7 +30,7 @@ public class NestEgg
 		for (int i = 1; i < years; i++)
 		{
 		F = F * (1+0.01 * growthRate) + salary * save * 0.01;
-		if (ans.equals("y"))
+		if (ans.equals("y")) //CHANGE FOR STRING ARGUMENTS
 			F = F * (1 - 0.01 * infla); //converts inflation input to decimal
 		System.out.println("End of year " + (i + 1) + ": $" + F);
 		}
